83275b8acecbb65374842cc2d61ae88a47dea853
Godsway Asamoah <gasamoah@wacci.ug.edu.gh>
2024-06-27 21:48:13 +0000

    My app commit

---
+++ frontend/src/components/App/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>Welcome to Duffy Care App</h1>
+            </header>
+            <p>
+                Start building your healthcare service application here!
+            </p>
+        </div>
+    );
+}
+
+export default App;
+
